dd4c280067cd88172fba2279c3820b5848bb7ecd
CyberJalagam <rbinternationalnetwork@gmail.com>
2021-02-21 17:07:46 +0000

    Revert "CPH1859: Add TetheringOverlay"
    This reverts commit 43c4b525daad598342c46396e63a8be1500d1e25.
    
    Signed-off-by: CyberJalagam <rbinternationalnetwork@gmail.com>

--- device.mk
+++ device.mk
@@ -130,11 +130,7 @@ PRODUCT_PACKAGES += \
     wpa_supplicant \
     wpa_supplicant.conf
 
-# Tethering
-PRODUCT_PACKAGES += \
-    TetheringConfigOverlay
-
-
+    
 # KPOC
 PRODUCT_PACKAGES += \
     libsuspend
